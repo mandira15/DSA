4f7a95f356b1e45fc5e88b47469a0dd1d65910cd
mandira15 <mandira15092005@gmaqil.com>
2026-04-25 00:02:14 +0530

    Added Number of Islands

---
+++ Grid/numberOfIslands.java
@@ -0,0 +1,48 @@
+class numberOfIslands {
+
+    void dfs(char[][] grid, int i, int j) {
+        // boundary + water check
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
+            return;
+        }
+
+        // mark visited
+        grid[i][j] = '0';
+
+        // explore 4 directions
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+
+        int m = grid.length;
+        int n = grid[0].length;
+        int islands = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    islands++;
+                }
+            }
+        }
+
+        return islands;
+    }
+    public static void main(String[] args) {
+        numberOfIslands solution = new numberOfIslands();
+        char[][] grid = {
+            {'1', '1', '0', '0', '0'},
+            {'1', '1', '0', '0', '0'},
+            {'0', '0', '1', '0', '0'},
+            {'0', '0', '0', '1', '1'}
+        };
+        int result = solution.numIslands(grid);
+        System.out.println("Number of islands: " + result); // Output: 3
+    }
+}
